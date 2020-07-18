2b02001568bde66047976b34f1dcfb93b38125eb
jkamz <jkamz6432@gmail.com>
2020-07-19 02:16:33 +0300

    Fixed message typo

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -10,7 +10,7 @@ namespace SignalRChat.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("RecieveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
 }
